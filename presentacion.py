7252d1f44784c77f695ee2c356d5ec930d1e64dd
Nahuel <cruznahuel4@hotmail.com>
2017-04-27 19:55:30 -0300

    Commit inicial del trabajo práctico. Se agrega la función de bienvenida.

---
+++ presentacion.py
@@ -0,0 +1,3 @@
+def mostrarMenuBienvenida():
+    print(" ¡¡¡¡ BIENVENIDO AL JUEGO LUCES AFUERA !!!!  ")
+    print("Seleccione la letra P para el modo predeterminado o la A para el modo aleatorio o la letra S para salir")
---
+++ principal.py
@@ -0,0 +1,3 @@
+import presentacion
+
+presentacion.mostrarMenuBienvenida();
